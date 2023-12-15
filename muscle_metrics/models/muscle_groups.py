cd45923cef4152436c3e093269e456d3bcba98a7
Chris Grabham <chrisgrabham@outlook.com>
2023-12-15 18:44:39 +0000

    Add Progress model

--- muscle_metrics/models/muscle_groups.py
+++ muscle_metrics/models/muscle_groups.py
@@ -5,9 +5,9 @@ class MuscleGroups(db.Model):
     __tablename__ = "muscle_groups"
     id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    exercises = db.relationship(
+        "Exercises", backref="muscle_groups", cascade="all, delete", lazy=True
+    )
 
     def __repr__(self):
-        return (
-            self.id,
-            self.name,
-        )
+        return (self.id, self.name, self.exercises)
