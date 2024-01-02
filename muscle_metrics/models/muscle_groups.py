50c276b68a205018887d9a42b79266cac3edf8d3
Chris Grabham <chrisgrabham@outlook.com>
2024-01-02 18:12:19 +0000

    Add name data to MuscleGroups

--- muscle_metrics/models/muscle_groups.py
+++ muscle_metrics/models/muscle_groups.py
@@ -11,3 +11,19 @@ class MuscleGroups(db.Model):
 
     def __repr__(self):
         return (self.id, self.name, self.exercises)
+
+
+muscle_group_1 = MuscleGroups(name="Arms")
+muscle_group_2 = MuscleGroups(name="Back")
+muscle_group_3 = MuscleGroups(name="Chest")
+muscle_group_4 = MuscleGroups(name="Core")
+muscle_group_5 = MuscleGroups(name="Legs")
+muscle_group_6 = MuscleGroups(name="Shoulders")
+
+db.session.add(muscle_group_1)
+db.session.add(muscle_group_2)
+db.session.add(muscle_group_3)
+db.session.add(muscle_group_4)
+db.session.add(muscle_group_5)
+db.session.add(muscle_group_6)
+db.session.commit()
