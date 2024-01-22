75e1e28d2f52cb410bd3f165b4686c8b3c82b153
Chris Grabham <chrisgrabham@outlook.com>
2024-01-22 17:28:00 +0000

    Add docstrings to routes

--- muscle_metrics/login/routes.py
+++ muscle_metrics/login/routes.py
@@ -7,21 +7,28 @@ from muscle_metrics.models import User
 from .forms import LoginForm
 
 
-# Login
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Handle the login process for a user.
+
+    This route allows users to log in to the application using their email and password.
+    It validates the user's credentials, logs them in upon success, and redirects to the dashboard.
+    If the login is unsuccessful, it flashes an error message and reloads the login page.
+
+    Returns:
+    Template or Redirection: Renders the login page template on GET request or invalid form submission.
+    Redirects to the dashboard upon successful login.
+    """
     form = LoginForm()
 
     # Form Validation
     if form.validate_on_submit():
         user: User = db.session.query(User).filter_by(email=form.email.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                flash("Login successful", "success")
-                return redirect(url_for("dashboard"))
-            else:
-                flash("Incorrect email or password. Please try again!", "error")
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("Login successful", "success")
+            return redirect(url_for("dashboard"))
         else:
             flash("Incorrect email or password. Please try again!", "error")
 
