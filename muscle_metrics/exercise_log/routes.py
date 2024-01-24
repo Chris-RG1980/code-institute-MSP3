f66758f2ba3ae50ad50d2944606b4844851a6424
Chris Grabham <chrisgrabham@outlook.com>
2024-01-24 18:58:00 +0000

    Return to dashboard after edit

--- muscle_metrics/exercise_log/routes.py
+++ muscle_metrics/exercise_log/routes.py
@@ -128,9 +128,10 @@ def log_edit(progress_id):
         form.muscle_group.data = progress.muscle_group_id
         form.exercises.data = progress.exercise_id
 
-    return render_template(
-        "exercise/exercises.html", form=form, isNew=False, progress_id=progress_id
-    )
+    # return render_template(
+    # "exercise/exercises.html", form=form, isNew=False, progress_id=progress_id
+    # )
+    return redirect(url_for("dashboard"))
 
 
 @app.route("/log/<int:progress_id>/delete", methods=["GET", "POST"])
